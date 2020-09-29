f8018e5bb55c8fdd92df4aff88b9871874914c06
JeDeschamps <jerome@LAPTOP-7EIT0NTC.localdomain>
2020-09-29 21:57:35 +0200

    Documentation de BehaviorFactory

--- GildedRose/src/main/java/com/gildedrose/behavior/BehaviorFactory.java
+++ GildedRose/src/main/java/com/gildedrose/behavior/BehaviorFactory.java
@@ -2,11 +2,42 @@ package com.gildedrose.behavior;
 import com.gildedrose.*;
 import com.gildedrose.conjured.*;
 
+/**
+ * Notre fabrique d'objets Behavior
+ *
+ * Instancie dynamiquement le comportement approprie a l'objet
+ * qu'on passe en parametre a notre unique methode.
+ * Utilise un switch sur le nom de l'item dans cette version.
+ * --> Trop de return au sein d'une meme fonction
+ *
+ * Methode statique, pas besoin d'instancier la factory pour l'utiliser
+ *
+ * @see Behavior
+ * @see Item
+ *
+ * @version 1.0
+ */
 public final class BehaviorFactory {
+  /**
+   * Constructeur prive, cree uniquement pour le checkstyle
+   *
+   * Corps de methode vide
+   */
   private BehaviorFactory() {
 
   }
 
+  /**.
+   * Methode statique de la fabrique
+   * Renvoie un comportement par defaut en cas d'item inconnu ou pas implemente
+   *
+   *
+   * @see Behavior
+   * @see Item
+   *
+   * @param it l'objet dont on souhaite obtenir le comportement associe
+   * @return Un objet behavior, comportement associe a l'objet
+   */
   public static Behavior getItemBehavior(Item it) {
     final String str = it.getName().substring(0, 8);
     switch (str) {
