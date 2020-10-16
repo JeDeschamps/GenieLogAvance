563951506377ea04ea3591538d3ea15c99abddb0
JeDeschamps <jerome@LAPTOP-7EIT0NTC.localdomain>
2020-10-16 16:31:22 +0200

    Fix doc

--- GildedRose/src/test/java/com/gildedrose/package-info.java
+++ GildedRose/src/test/java/com/gildedrose/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Ce package contient l'ensemble de nos classes de tests.
- * Les tests ne sont pas 100% exhaustifs, etant donne l'implementation
- * il n'est pas utile de tester chaque comportement pour chaque item.
- * Les tests ne seraient pas plus pertinents en couvrant tous les cas possibles.
+ * Les tests fournissent une couverture a 100% des instructions
+ * Toute la semantique possible de degradation et augmentation de la qualite
+ * et du sellIn est testée
  *
  * On se fie sur l'outil jacocoTestCoverageVerification pour s'assurer que toutes
  * les instructions sont toutes evaluees.
